52f76d00f11032127b687f9afc62632383cc3ca7
Somnath11111 <69788891+Somnath11111@users.noreply.github.com>
2025-01-26 13:12:34 +0530

    check dependabot PR

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ async function run() {
   const { context } = github;
   console.log(`PR Title: ${context.payload.pull_request.title}`);
   console.log(`PR Author: ${context.payload.pull_request.user.login}`);
+
+  if (context.payload.pull_request.user.login === "dependabot[bot]") {
+    console.log("This PR is raised by Dependabot.");
+  } else {
+    console.log("Not a Dependabot PR.");
+  }
+
+  
 }
 
 run().catch((error) => {
